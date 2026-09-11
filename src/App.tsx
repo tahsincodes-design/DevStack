0d596e02476130bf92b463e9849e18a548e363f6
tahsincodes-design <ahmed.tahsin.1155@gmail.com>
2026-09-11 21:49:38 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+function App() {
+  
+  return (
+    <>
+      <div className="p-10">
+      <button className="btn btn-primary">DevStack Button</button>
+      </div>
+    </>
+  )
+}
+
+export default App
